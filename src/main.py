7b65e137d3d8b8bb262af8d660dd781b6d492bc4
dongsikchoi <dongsikchoi.dev@gmail.com>
2023-08-14 17:33:42 +0900

    [ADD] docker-compose.yml

--- src/main.py
+++ src/main.py
@@ -1,14 +1,22 @@
 from typing import Union
-
+import redis
 from fastapi import FastAPI
 
 app = FastAPI()
 
+r = redis.Redis(host="redis",port=6379)
 
+import debugpy
+debugpy.listen(("0.0.0.0",5678))
+debugpy.wait_for_client()
 @app.get("/")
 def read_root():
-    return {"Hello": "TESTTTT"}
+    return {"Hello": "dschdoi"}
 
+@app.get("/hits")
+def read_root():
+    r.incr("hits")
+    return {"Numbewr of hits": r.get("hits")}
 
 # @app.get("/items/{item_id}")
 # def read_item(item_id: int, q: Union[str, None] = None):
